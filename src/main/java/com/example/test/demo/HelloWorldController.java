c2a23db54b6ec371c1d966e146837b1a5eefbf8f
bernie <bernie.xu@oocl.com>
2019-03-22 22:17:15 +0800

    first commit

---
+++ src/main/java/com/example/test/demo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.test.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot HelloWorld案例
+ *
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/aa")
+    public String sayHello() {
+        return "Hello,World!????";
+    }
+}
